280a98e647c1b9beb940610465df542b5eeed5b2
lstarrsmith <logan29smith@gmail.com>
2016-02-14 13:07:58 +0300

    updated s3_protocol => https in the image model file

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,7 +3,9 @@ class Image < ActiveRecord::Base
 
 	has_attached_file :photo, :styles => { :thumb => "100x100", :medium => "300x300", :large => "600x600"},
 		:url => "loganportfolio.s3.amazonaws.com",
-		:path => '/:class/:attachment/:id_partition/:style/:filename'
+		:path => '/:class/:attachment/:id_partition/:style/:filename',
+		# http://stackoverflow.com/questions/7897710/is-it-possible-to-configure-paperclip-to-produce-https-urls
+		:s3_protocol => :https
 
 		# :path => ":rails_root/public/assets/images/:id/:filename",
 		# :url => "/assets/images/:id/:filename"
